031579dbf02466e1f2174cee5a4dead9acc5cedd
Arif14377 <52228176+Arif14377@users.noreply.github.com>
2025-12-26 15:37:58 +0700

    Add files via upload

--- music-group.ps1
+++ music-group.ps1
@@ -1,45 +1,77 @@
+$artis = @()
+$lagu = @()
+
 #get artis
 $artis = Get-ChildItem -Path ".\Music\" -Directory -Name
 $lagu = Get-ChildItem -Path ".\Music\*" -Name -Recurse
 
-# echo $artis
-# echo $lagu
+$banyakFolder = $artis.Length
+$banyakLagu = $lagu.Length
+# echo $banyakFolder
+echo $banyakLagu
+# echo $artis[1]
+# echo $lagu[1]
+# $laguToMove
 
-#### Bondan
-$laguBondan = $lagu | Where-Object {$_ -match "Bondan Prakoso"}
-# echo $laguBondan
+$folderArtis
 
-$folderBondan = $artis | Where-Object {$_ -match "Bondan Prakoso"}
-# echo $folderBondan
+for($i=0; $i -lt $banyakFolder; $i++){
+    $folderArtis = $artis[$i]
 
-foreach ($lb in $laguBondan){
-    Move-Item -Path ".\Music\$lb" -Destination ".\Music\$folderBondan\"
+    for($x=0; $x -lt $banyakLagu; $x++) {
+        $laguToMove = $lagu[$x]
+        
+        if($laguToMove -match $folderArtis) {
+        Copy-Item -Path ".\Music\$laguToMove" -Destination ".\Music\$folderArtis\"
+    }
+    }
+    
+    # echo "Folder ke-$i adalah: "$folderArtis
+    # echo "lagu ke-$i adalah: " $laguToMove
 }
 
-### Iwan Fals
-$laguIwan = $lagu | Where-Object {$_ -match "Iwan Fals"}
-$folderIwan = $artis | Where-Object {$_ -match "Iwan Fals"}
-foreach ($li in $laguIwan){
-    Move-Item -Path ".\Music\$li" -Destination ".\Music\$folderIwan\"
-}
+Remove-Item -Path .\Music\*.mp3
 
-### ST-12
-$laguST12 = $lagu | Where-Object {$_ -match "ST-12"}
-$folderST12 = $artis | Where-Object {$_ -match "ST-12"}
-foreach ($ls in $laguST12){
-    Move-Item -Path ".\Music\$ls" -Destination ".\Music\$folderST12\"
-}
 
-### Tipe-X
-$laguTpx = $lagu | Where-Object {$_ -match "Tipe-X"}
-$folderTpx = $artis | Where-Object {$_ -match "Tipe-X"}
-foreach ($lt in $laguTpx){
-    Move-Item -Path ".\Music\$lt" -Destination ".\Music\$folderTpx\"
-}
 
-### Wali
-$laguW = $lagu | Where-Object {$_ -match "Wali"}
-$folderW = $artis | Where-Object {$_ -match "Wali"}
-foreach ($lw in $laguW){
-    Move-Item -Path ".\Music\$lw" -Destination ".\Music\$folderW\"
-}
+
+
+
+# #### Bondan
+# $laguBondan = $lagu | Where-Object {$_ -match "Bondan Prakoso"}
+# # echo $laguBondan
+
+# $folderBondan = $artis | Where-Object {$_ -match "Bondan Prakoso"}
+# # echo $folderBondan
+
+# foreach ($lb in $laguBondan){
+#     Move-Item -Path ".\Music\$lb" -Destination ".\Music\$folderBondan\"
+# }
+
+# ### Iwan Fals
+# $laguIwan = $lagu | Where-Object {$_ -match "Iwan Fals"}
+# $folderIwan = $artis | Where-Object {$_ -match "Iwan Fals"}
+# foreach ($li in $laguIwan){
+#     Move-Item -Path ".\Music\$li" -Destination ".\Music\$folderIwan\"
+# }
+
+# ### ST-12
+# $laguST12 = $lagu | Where-Object {$_ -match "ST-12"}
+# $folderST12 = $artis | Where-Object {$_ -match "ST-12"}
+# foreach ($ls in $laguST12){
+#     Move-Item -Path ".\Music\$ls" -Destination ".\Music\$folderST12\"
+# }
+
+# ### Tipe-X
+# $laguTpx = $lagu | Where-Object {$_ -match "Tipe-X"}
+# $folderTpx = $artis | Where-Object {$_ -match "Tipe-X"}
+# foreach ($lt in $laguTpx){
+#     Move-Item -Path ".\Music\$lt" -Destination ".\Music\$folderTpx\"
+# }
+
+# ### Wali
+# $laguW = $lagu | Where-Object {$_ -match "Wali"}
+# $folderW = $artis | Where-Object {$_ -match "Wali"}
+# foreach ($lw in $laguW){
+#     Move-Item -Path ".\Music\$lw" -Destination ".\Music\$folderW\"
+# }
